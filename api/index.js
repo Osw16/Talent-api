51a31251e8ecd10328177e4cb92a6a775683ba22
rxcket <oswaldoj16@gmail.com>
2023-01-12 15:47:26 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,10 @@
+// In Vercel, any file inside the "api" directory is exposed on an "/api" endpoint.
+
+// For an API route to work, you need to export a function as default (a.k.a request handler),
+// which then receives the following parameters:
+// - req: The request object.
+// - res: The response object.
+// See https://vercel.com/docs/serverless-functions/supported-languages#node.js for details.
+export default async function handler(req, res) {
+    res.status(200).send(`Hello world!`);
+  }
